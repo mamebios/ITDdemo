9d64e3ed818972a35b89ec8def97446eb147811c
mamebios <bruno.mamedio.rodrigues@gmail.comexitsssljasdjlkjquitgit config --global user.email bruno.mamedio.rodrigues@gmail.com>
2024-01-31 16:18:21 -0300

    Testing

---
+++ audio.py
@@ -0,0 +1,40 @@
+import time
+
+import numpy as np
+import pyaudio
+
+p = pyaudio.PyAudio()
+
+volume = 0.5  # range [0.0, 1.0]
+fs = 44100  # sampling rate, Hz, must be integer
+duration = 5.0  # in seconds, may be float
+f = 440.0  # sine frequency, Hz, may be float
+
+# generate samples, note conversion to float32 array
+samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+
+# per @yahweh comment explicitly convert to bytes sequence
+output_bytes = (volume * samples).tobytes()
+
+stereo_signal = np.zeros([len(samples), 2])
+stereo_signal[:, 1] = samples[:]
+stereo_bytes = (stereo_signal * volume).tobytes()
+
+# for paFloat32 sample values must be in range [-1.0, 1.0]
+stream = p.open(format=pyaudio.paFloat32,
+                channels=2,
+                rate=fs,
+                output=True)
+
+# play. May repeat with different volume values (if done interactively)
+start_time = time.time()
+
+#stream.write(output_bytes)
+stream.write(stereo_bytes)
+
+print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
